e80fe7938e6104569455d1e9fc285c927c6b9dd2
Michael Lee <mlee16@hawk.iit.edu>
2016-07-18 19:13:39 -0800

    Restyled minor details and added some jquery into script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,26 @@
+function convertColor(e) {
+    $.post('/convert', $('.input').serialize(), function(color) {
+        alert('');
+    }, 'text');
+    e.stopPropagation();
+}
+
+function chooseColor(e) {
+    $.get('/choose', function(colors) {
+        alert('loaded chosen colors');
+        console.log(colors);
+    }, 'json');
+    e.stopPropagation();
+}
+
+function chooseRandomColor(e) {
+    $.get('/random', function(colors) {
+        alert('loaded random colors');
+        console.log(colors);
+    }, 'json');
+    e.stopPropagation();
+}
+
 $(document).ready(function() {
     $('#pagepiling').pagepiling({
         sectionsColor: ['#f9f9f9', '#2860BF'],
@@ -9,4 +32,9 @@ $(document).ready(function() {
     $('.about').click(function() {
         $.fn.pagepiling.moveSectionDown();
     });
+
+    $('.choose').one('click', chooseColor);
+
+    $('.random').on('click', chooseRandomColor);
+
 });
